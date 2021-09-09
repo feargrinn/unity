0da0f037e8946a87a2cd43595e98f0bc4d9585eb
feargrinn <64360466+feargrinn@users.noreply.github.com>
2021-09-09 23:22:34 +0200

    Add files via upload

---
+++ box-collider/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+public class PlayerCollision : MonoBehaviour
+{
+    public player_movement movement;
+    void OnCollisionEnter(Collision collisionInfo){
+        if (collisionInfo.collider.tag == "obstacle"){
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
---
+++ box-collider/Assets/Scripts/player_movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public Transform player;
+    public float forwardForce = 400f; //f because double (d) is the default float datatype in c# but unitys default is float so we force C# compiler to treat this as float by adding f
+    public float sidewaysForce = 400f;
+
+    // Update is called once per frame
+    //FixedUpdate because Unity likes it more when we mess with physics
+    void FixedUpdate()
+    {
+        //rb.AddForce(forwardForce * Time.deltaTime, 0, 0); //added force on x axis, Time.deltaTime is 1/FPS to even out the force on all devices with different framerates
+
+        if( Input.GetKey("d") ){
+            rb.AddForce(0, 0, -sidewaysForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if( Input.GetKey("a") ){
+            rb.AddForce(0, 0, sidewaysForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if( Input.GetKey("w") ){
+            rb.AddForce(forwardForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if( Input.GetKey("s") ){
+            rb.AddForce(-forwardForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if(rb.position.y < -1f){
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
